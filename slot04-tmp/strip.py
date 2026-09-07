0c5b2ae3a2bbd0407ffd126b184e597488feb9e6
nicklynch10 <nicklynch10@users.noreply.github.com>
2026-09-07 18:49:53 -0400

    chore: remove battery-runner scratch (slot04-tmp stripped page copies) [skip ci]

--- slot04-tmp/strip.py
+++
@@ -1,9 +0,0 @@
-import re,html,sys
-raw=open(sys.argv[1],encoding='utf-8').read()
-raw=re.sub(r'<script.*?</script>',' ',raw,flags=re.S|re.I)
-raw=re.sub(r'<style.*?</style>',' ',raw,flags=re.S|re.I)
-raw=re.sub(r'<[^>]+>',' ',raw)
-text=html.unescape(re.sub(r'[ \t\xa0]+',' ',raw))
-text=re.sub(r'\n\s*\n+','\n',text)
-open(sys.argv[2],'w',encoding='utf-8').write(text)
-print(len(text))
